56027ddf1fc22a8cc7b1dcea68e56edb5dd45dd8
pseudobot <shivamx6174@gmail.com>
2021-07-14 03:17:08 +0530

    side bar

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const item1 = document.querySelector('.m1');
+const item2 = document.querySelector('.m2');
+const item3 = document.querySelector('.m3');
+const item4 = document.querySelector('.m4');
+const item5 = document.querySelector('.m5');
